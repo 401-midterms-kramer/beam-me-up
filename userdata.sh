b7c20828c042b7ac6531c88d26a4a66baeca7f16
Lee-Roy King <leeroywking@gmail.com>
2019-09-08 13:13:20 -0700

    added node packages

--- userdata.sh
+++ userdata.sh
@@ -4,3 +4,5 @@ curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt install curl -y
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt install nodejs -y
+git clone https://github.com/401-midterms-kramer/beam-me-up_server
+npm i pm2 -g
