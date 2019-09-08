89603f038fec333466472d7c9c51ec03169bcf21
Lee-Roy King <leeroywking@gmail.com>
2019-09-08 13:10:43 -0700

    added userdata script v0.1

---
+++ userdata.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt install curl -y
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt install curl -y
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt install nodejs -y
